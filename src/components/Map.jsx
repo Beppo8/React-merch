6cd6203f17a66d097b6a802a8eefd6142d6697b8
Beppo <beppo2488sm@gmail.com>
2020-11-24 02:50:16 -0600

    Add GoogleMaps API & Custom Hooks

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import {}
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-const Map = () => {
+const Map = ({ data }) => {
+
+    const mapStyles = {
+        height: "50vh",
+        width: "100%"
+    }
+
+    const defaultCenter = {
+        lat: data.lat, lng: data.lng
+    }
+
     return (
-        
-    )
+        <LoadScript googleMapsApiKey='asdasfdsafasf'>
+            <GoogleMap
+                mapContainerStyle={mapStyles}
+                zoom={9}
+                center={defaultCenter}
+            >
+                <Marker position={defaultCenter} />
+            </GoogleMap>
+        </LoadScript>
+    );
 }
 
-export default Map
+export default Map;
